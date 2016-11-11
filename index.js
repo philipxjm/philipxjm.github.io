dff28d0b3f8a9ff4f611af50a6559da983c639b8
Philip Xu <philipxjm1@gmail.com>
2016-11-11 01:23:28 -0500

    added aspect ratio balance for mobile

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ function run() {
     image.crossOrigin = 'anonymous';
     image.src = 'http://i.imgur.com/x86Mup0.jpg';
 }
+
+$(window).load(function() {
+
+    var theWindow = $(window),
+        $background = $("#background"),
+        aspectRatio = $background.width() / $background.height();
+
+    function resizeBg() {
+
+        if ((theWindow.width() / theWindow.height()) < aspectRatio) {
+            $background
+                .removeClass()
+                .addClass('bgheight');
+        } else {
+            $background
+                .removeClass()
+                .addClass('bgwidth');
+        }
+
+    }
+
+    theWindow.resize(resizeBg).trigger("resize");
+
+});
